a357462d4cae70e2a99ca835115f1bea80d32f32
Glacialice <regnumforce@gmail.com>
2023-10-10 14:40:36 +0700

    soal 2 ubah MyGarden menjadi horizontal list dengan 3 kolom

--- app/src/main/java/com/google/samples/apps/sunflower/compose/garden/GardenScreen.kt
+++ app/src/main/java/com/google/samples/apps/sunflower/compose/garden/GardenScreen.kt
@@ -97,7 +97,7 @@ private fun GardenList(
     val gridState = rememberLazyGridState()
     ReportDrawnWhen { gridState.layoutInfo.totalItemsCount > 0 }
     LazyVerticalGrid(
-        columns = GridCells.Fixed(2), // Ubah ini
+        columns = GridCells.Fixed(3), // Perubahan untuk nomor 2
         modifier,
         state = gridState,
         contentPadding = PaddingValues(
