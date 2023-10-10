20e078c7835d196c5121a149d9c0788cd604dd04
Glacialice <regnumforce@gmail.com>
2023-10-10 14:48:26 +0700

    Soal 3 ubah card menjadi full rounded ukuran 32dp

--- app/src/main/java/com/google/samples/apps/sunflower/ui/Shapes.kt
+++ app/src/main/java/com/google/samples/apps/sunflower/ui/Shapes.kt
@@ -20,6 +20,10 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Shapes
 import androidx.compose.ui.unit.dp
 
+/**
+ * Perubahan nomor 3 yaitu ubah yang bagian medium
+ * ubah semua yang medium menjadi 32.dp
+*/
 val Shapes = Shapes(
     small = RoundedCornerShape(
         topStart = 0.dp,
@@ -28,9 +32,9 @@ val Shapes = Shapes(
         bottomEnd = 0.dp
     ),
     medium = RoundedCornerShape(
-        topStart = 0.dp,
-        topEnd = 12.dp,
-        bottomStart = 12.dp,
-        bottomEnd = 0.dp
+        topStart = 32.dp,
+        topEnd = 32.dp,
+        bottomStart = 32.dp,
+        bottomEnd = 32.dp
     )
 )
